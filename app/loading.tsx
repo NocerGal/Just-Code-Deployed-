153f86667464c54095b0984431fbae4906fedf86
NocerGal <luc.schenherr@hotmail.fr>
2024-02-18 09:19:11 +0100

    Correction pour deploy

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Loading() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
+    <div className="flex h-full w-full items-center justify-center">
       <Loader className="animate-spin" size={32} />
     </div>
   );
