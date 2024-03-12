6fe9d918e9b13a8f2e8ad5633433c4b5d458974c
NocerGal <luc.schenherr@hotmail.fr>
2024-03-12 15:28:29 +0100

    retrait de l'image de caracpuce dans le footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,7 +10,6 @@ export const Footer = () => {
       <div className="m-auto w-full max-w-3xl px-2 py-4">
         <div className="flex flex-col lg:flex-row lg:items-start lg:justify-between">
           <div className="flex flex-row items-center gap-2">
-            <Image src={Logo} width={40} height={30} alt="app logo" />
             <Typography variant="base" as={Link} href="/">
               {SiteConfig.title}
             </Typography>
